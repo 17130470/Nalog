f3dcbd5093f0b38aa204b222a7d70208b86dbdaa
Миша <@LAPTOP-HLA217QP>
2019-05-13 22:32:33 +0500

    мелочи

--- src/test/java/nalog/nak/test2.java
+++ src/test/java/nalog/nak/test2.java
@@ -1,4 +1,4 @@
-package nalog.nak;
+package nalog.nak; 
 /*Подключение библиотеки. JUnit - это библиотека, позволяющая
  * проводить модульное тестирование Java приложений. Класс Assert 
  * содержит набор утверждений, которые облегачют процесс тестирования. */
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 /*Импортирует из пакета NalogC класс tax*/
 import nalog.nak.tax;
-/*Объявляем новый класс test1*/
+/*Объявляем новый класс test2*/
 public class test2 {
 	/*Обозначает новую переменную из класса tax*/
 	tax tax = new tax();
@@ -15,12 +15,12 @@ public class test2 {
 	@Test
 	public void testTax() throws InterruptedException
 	 {
-		/*Подставляет значения в метод calcTax1,
-		 * где высчитывают процент налога с дохода*/
-	 double res= tax.calcTax(1000, 13);
+		/*Подставляет значения в метод calcTax2,
+		 * где высчитывают деньги полученные на руки после вычета налога */
+	 double res= tax.calcTax2(1000, 13);
 		/*Заданное полученное значение*/
-	 double toch=130.0;
-		/*Сравнивают высчитанное методом calcTax и заранее заданное значение */
+	 double toch=870.0;
+		/*Сравнивают высчитанное методом calcTax2 и заранее заданное значение */
 	 assertEquals(Double.toString(res),Double.toString(toch));
-	 }
-}
+	 }	
+} 
--- src/test/java/nalog/nak/test3.java
+++ src/test/java/nalog/nak/test3.java
@@ -1,4 +1,4 @@
-package nalog.nak;
+package nalog.nak; 
 /*Подключение библиотеки. JUnit - это библиотека, позволяющая
  * проводить модульное тестирование Java приложений. Класс Assert 
  * содержит набор утверждений, которые облегачют процесс тестирования. */
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 /*Импортирует из пакета NalogC класс tax*/
 import nalog.nak.tax;
-/*Объявляем новый класс test1*/
+/*Объявляем новый класс test3*/
 public class test3 {
 	/*Обозначает новую переменную из класса tax*/
 	tax tax = new tax();
@@ -17,9 +17,9 @@ public class test3 {
 	 {
 		/*Подставляет значения в метод calcTax1,
 		 * где высчитывают процент налога с дохода*/
-	 double res= tax.calcTax(1000, 13);
+	 double res= tax.calcTax(100000, 15);
 		/*Заданное полученное значение*/
-	 double toch=130.0;
+	 double toch=15000.0;
 		/*Сравнивают высчитанное методом calcTax и заранее заданное значение */
 	 assertEquals(Double.toString(res),Double.toString(toch));
 	 }
